e4455d01e6f94ff5a2e0b369b9f12f00b01e4b53
jinabro <kmy4869@naver.com>
2022-05-29 14:46:23 +0900

    ~BFS

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -5,53 +5,96 @@
 #include <queue>
 using namespace std;
 
-void CreateGraph_1()
+// [ ][ ][ ][ ][ ][ ][ ][ ]
+
+// DFS (Depth First Search) 깊이 우선 탐색
+// BFS (Breadth First Search) 너비 우선 탐색
+
+struct Vertex
 {
-	struct Vertex
+	// int data;
+};
+
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+vector<bool> discoverd;
+
+void CreateGraph()
+{
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6);
+
+	// 인접 리스트
+	adjacent[0].push_back(1);
+	adjacent[0].push_back(3);
+	adjacent[1].push_back(0);
+	adjacent[1].push_back(2);
+	adjacent[1].push_back(3);
+	adjacent[3].push_back(4);
+	adjacent[5].push_back(4);
+
+	// 인접 행렬
+	//adjacent = vector<vector<int>>
+	//{
+	//	{ 0, 1, 0, 1, 0, 0},
+	//	{ 1, 0, 1, 1, 0, 0},
+	//	{ 0, 0, 0, 0, 0, 0},
+	//	{ 0, 0, 0, 0, 1, 0},
+	//	{ 0, 0, 0, 0, 0, 0},
+	//	{ 0, 0, 0, 0, 1, 0},
+	//};
+}
+
+void Bfs(int here)
+{
+	// 누구에 의해서 발견 되었는지?
+	vector<int> parent(6, -1);
+	// 시작점에서 얼만큼 떨어져 있는지?
+	vector<int> distance(6, -1);
+
+
+	// 발견한 순서를 넣어주는 예약시스템 큐
+	queue<int> q;
+	q.push(here);
+	discoverd[here] = true;
+
+	parent[here] = here;
+	distance[here] = 0;
+
+	while (q.empty() == false)
 	{
-		// 간선 목록
-		vector<Vertex*> edges;
-	};
+		here = q.front();
+		q.pop();
 
-	vector<Vertex> v;
-	v.resize(6);
+		cout << "visited : " << here << endl;
 
-	v[0].edges.push_back(&v[1]);
-	v[0].edges.push_back(&v[3]);
-	v[1].edges.push_back(&v[0]);
-	v[1].edges.push_back(&v[2]);
-	v[1].edges.push_back(&v[3]);
-	v[3].edges.push_back(&v[4]);
-	v[5].edges.push_back(&v[4]);
-
-	// Q) 0번 -> 3번 정점이 연결되어 있나요?
-	bool connected = false;
-	for (Vertex* edge : v[0].edges)
-	{
-		if (edge == &v[3])
+		for (int there: adjacent[here])
 		{
-			connected = true;
-			break;
+			if (discoverd[there])
+				continue;
+
+			q.push(there);
+			discoverd[there] = true;
+
+			parent[there] = here;
+			distance[there] = distance[here] + 1;
 		}
 	}
 }
 
-
-void CreateGraph_2()
+void BfsAll()
 {
-	struct Vertex
-	{
-	};
-
-	vector<Vertex> v;
-	v.resize(6);
-
-	// 연결된 목록 따로 관리
-	vector<vector<int>> adjacent(6);
+	for (int i = 0; i < 6; i++)
+		if (discoverd[i] == false)
+			Bfs(i);
 }
+
 
 int main()
 {
-	CreateGraph_1();
+	CreateGraph();
 
+	discoverd = vector<bool>(6, false);
+
+	BfsAll();
 }
