a81381af848c7fb55aa3591a76cff4df97413691
jinabro <kmy4869@naver.com>
2022-07-04 22:35:59 +0900

    오른손 법칙

--- Algorithm/Maze/Player.cpp
+++ Algorithm/Maze/Player.cpp
@@ -1,24 +1,93 @@
 #include "pch.h"
 #include "Player.h"
 #include "Board.h"
-#include <stack>
-// 배열 (Array), 동적 배열(Vector), 연결 리스트(List)
 
 void Player::Init(Board* board)
 {
+	//_pos 는 플레이어의 진짜 위치
 	_pos = board->GetEnterPos();
 	_board = board;
 
-	//RightHand();
-	Bfs();
+	// 이동 하는 시뮬레이션 하기위한 임시 좌표
+	Pos pos = _pos;
+
+	// 두번 호출할까봐
+	_path.clear();
+	// 제일 처음 위치도 벡터에 넣어두기
+	_path.push_back(pos);
+
+	// 목적지 도착하기 전에는 계속 실행
+	Pos dest = board->GetExitPos();
+
+	Pos front[4] =
+	{
+		Pos { -1, 0},	// UP
+		Pos { 0, -1},	// LEFT
+		Pos { 1, 0},	// DOWN
+		Pos { 0, 1},	// RIGHT
+	};
+
+	// 목적지에 도달하기 전까지 실행
+	while (pos != dest)
+	{
+		// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
+		// _dir - 1 하면 오른쪽 방향.
+		int32 newDir = (_dir - 1 + DIR_COUNT) % DIR_COUNT;
+		// 오른쪽 방향으로 갈 수 있다면 
+		// CanGo : 넣어준 Pos가 Empty인지 반환
+		if (CanGo(pos + front[newDir]))
+		{
+			// 오른쪽 방향으로 90도 회전.
+			_dir = newDir;
+			// 앞으로 한 보 전진.
+			pos += front[_dir];
+
+			// 이동하면 가는 경로 벡터에 넣어주기
+			_path.push_back(pos);
+		}
+		// 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
+		// _dir은 플레이어가 현재 바라보고 있는 방향
+		else if (CanGo(pos + front[_dir]))
+		{
+			// 앞으로 한 보 전진.
+			pos += front[_dir];
+
+			_path.push_back(pos);
+		}
+		else
+		{
+			// 왼쪽 방향으로 90도 회전.
+			_dir = (_dir + 1) % DIR_COUNT;
+			/*
+			switch (_dir)
+			{
+			case DIR_UP:
+				_dir = DIR_LEFT;
+				break;
+			case DIR_LEFT:
+				_dir = DIR_DOWN;
+				break;
+			case DIR_DOWN:
+				_dir = DIR_RIGHT;
+				break;
+			case DIR_RIGHT:
+				_dir = DIR_UP;
+				break;
+			}
+			*/
+		}
+	}
 }
 
 void Player::Update(uint64 deltaTick)
 {
+	// path경로 다 소모
 	if (_pathIndex >= _path.size())
 		return;
 
 	_sumTick += deltaTick;
+
+	// 0.1초가 경과됨
 	if (_sumTick >= MOVE_TICK)
 	{
 		_sumTick = 0;
@@ -32,125 +101,4 @@ bool Player::CanGo(Pos pos)
 {
 	TileType tileType = _board->GetTileType(pos);
 	return tileType == TileType::EMPTY;
-}
-
-void Player::RightHand()
-{
-	Pos pos = _pos;
-
-	_path.clear();
-	_path.push_back(pos);
-
-	// 목적지 도착하기 전에는 계속 실행
-	Pos dest = _board->GetExitPos();
-
-	Pos front[4] =
-	{
-		Pos { -1, 0},	// UP
-		Pos { 0, -1},	// LEFT
-		Pos { 1, 0},	// DOWN
-		Pos { 0, 1},	// RIGHT
-	};
-
-	while (pos != dest)
-	{
-		// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
-		int32 newDir = (_dir - 1 + DIR_COUNT) % DIR_COUNT;
-		if (CanGo(pos + front[newDir]))
-		{
-			// 오른쪽 방향으로 90도 회전.
-			_dir = newDir;
-			// 앞으로 한 보 전진.
-			pos += front[_dir];
-
-			_path.push_back(pos);
-		}
-		// 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
-		else if (CanGo(pos + front[_dir]))
-		{
-			// 앞으로 한 보 전진.
-			pos += front[_dir];
-
-			_path.push_back(pos);
-		}
-		else
-		{
-			// 왼쪽 방향으로 90도 회전.
-			_dir = (_dir + 1) % DIR_COUNT;
-
-		}
-	}
-
-	// 돌아가는길 제거하기 위해 스택에 넣기
-	stack<Pos> s;
-	for (int i = 0; i < _path.size() - 1; i++)
-	{
-		if (s.empty() == false && s.top() == _path[i + 1])
-			s.pop();
-		else
-			s.push(_path[i]);
-	}
-
-	// 목적지 도착
-	if (_path.empty() == false)
-		s.push(_path.back());
-
-	vector<Pos> path;
-	while (s.empty() == false)
-	{
-		path.push_back(s.top());
-		s.pop();
-	}
-
-	std::reverse(path.begin(), path.end());
-
-	_path = path;
-}
-
-void Player::Bfs()
-{
-	Pos pos = _pos;
-
-
-
-	// 목적지 도착하기 전에는 계속 실행
-	Pos dest = _board->GetExitPos();
-
-	Pos front[4] =
-	{
-		Pos { -1, 0},	// UP
-		Pos { 0, -1},	// LEFT
-		Pos { 1, 0},	// DOWN
-		Pos { 0, 1},	// RIGHT
-	};
-
-	const int32 size = _board->GetSize();
-	vector<vector<bool>> discoverd(size, vector<bool>(size, false));
-
-	queue<Pos> q;
-	q.push(pos);
-	discoverd[pos.y][pos.x] = true;	
-
-	while (q.empty() == false)
-	{
-		pos = q.front();
-		q.pop();
-
-		// 방문!
-		if (pos == dest)
-			break;
-
-		for (int32 dir = 0; dir < 4; dir++)
-		{
-			Pos nextPos = pos + front[dir];
-			if (CanGo(nextPos) == false)
-				continue;
-
-
-		}
-	}
-
-	// TODO
-	_path.clear();
-	_path.push_back(pos);
 }
--- Algorithm/Maze/Player.h
+++ Algorithm/Maze/Player.h
@@ -1,11 +1,12 @@
 #pragma once
 
 class Board;
+
 class Player
 {
 	enum
 	{
-		MOVE_TICK = 100,
+		MOVE_TICK = 100
 	};
 
 public:
@@ -14,20 +15,19 @@ public:
 
 	void		SetPos(Pos pos) { _pos = pos; }
 	Pos			GetPos() { return _pos; }
-	
+
 	bool		CanGo(Pos pos);
 
 private:
-	void		RightHand();
-	void		Bfs();
-
-private:
-	Pos			_pos = { };
+	Pos			_pos = {};
 	int32		_dir = DIR_UP;
-	Board*		_board = nullptr;
+	Board* _board = nullptr;
 
-	vector<Pos> _path;
+	// Init에서 이동할 경로 다 계산 해서 push_back 해 놓을 것임, 업데이트에서 하나씩 소모하면서 이동할것
+	vector<Pos>	_path;
+	// 현재 vector에 들어간 경로 기준으로 어디를 이동하고있는지 체크
 	uint32		_pathIndex = 0;
-	uint32		_sumTick = 0;
+	// 현재까지의 Tick
+	uint64		_sumTick = 0;
 };
 
